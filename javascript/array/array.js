d0afce31e5badffd3ec70ecf77b7d04f2ddd2e18
Alessandro Giaquinto <alessandro.giaquinto87@gmail.com>
2023-11-27 23:27:25 +0100

    improvements

--- javascript/array/array.js
+++ javascript/array/array.js
@@ -40,7 +40,7 @@ class Rectangle {
 function _makeSquares() {
     let array = [];
 
-    for (let i = 1; i <= 10; i++) {
+    for (let i = 2; i <= 4; i++) {
         array.push(new Square(i));
     }
 
@@ -50,8 +50,8 @@ function _makeSquares() {
 function _makeRectangles() {
     let array = [];
 
-    for (let i = 1; i <= 5; i++) {
-        for (let j = 5; j <= 10; j++) {
+    for (let i = 1; i <= 2; i++) {
+        for (let j = 3; j <= 4; j++) {
             array.push(new Rectangle(i,j));
         }
     }
@@ -59,49 +59,112 @@ function _makeRectangles() {
     return array;
 }
 
-function createList(arr, useImportantOnLast) {
-    const returned = document.createElement("ul");
+function makeTextContent(i) {
+    if (i.base === undefined) {
+        return `Square side: ${i.side}, perimeter: ${i.perimeter}, area: ${i.area}`;
+    } else {
+        return `Rect base: ${i.base}, height: ${i.height}, perimeter: ${i.perimeter}, area: ${i.area}`;
+    }
+}
+
+function createList(arr, useImportantOnLast, useImportantOnFirst) {
+    const container = document.createElement("div");
+    const ul = document.createElement("ul");
+
+    container.appendChild(ul);
     let element;
-    let string = "";
 
     arr.forEach((i, idx) => {
         console.log(i);
         element = document.createElement("li");
 
-        if (i.base === undefined) {
-            element.textContent = `Square side: ${i.side}, perimeter: ${i.perimeter}, area: ${i.area}`;
-        } else {
-            element.textContent = `Rect base: ${i.base}, height: ${i.height}, perimeter: ${i.perimeter}, area: ${i.area}`;
-        }
+        element.textContent = makeTextContent(i);
 
-        if (useImportantOnLast && idx === arr.length-1) {
+        if (
+            (useImportantOnLast && idx === arr.length-1) ||
+            (useImportantOnFirst && idx === 0)
+        ) {
             element.setAttribute("class", "important-marker");
         }
 
-        returned.appendChild(element);        
+        ul.appendChild(element);        
     })
-
-    return returned;
+    
+    return container;
 }
 
-function setup() {
-    console.log("start array");
+function listSection() {
+    console.log('Before list', "make array");
 
-    const rects = _makeRectangles();
-    const squares = _makeSquares();
+    const arr = [..._makeRectangles(), ..._makeSquares()];
+    document.getElementById('list').appendChild(createList(arr, false, false));
 
-    console.log(rects);
-    console.log(squares);
+    console.log('After list', arr);
 
-    const arr = [...rects, ...squares];
-    console.log(arr);
+    return arr;
+}
 
-    document.getElementById('list').appendChild(createList(arr, false));
+function pushSection(arr) {
+    console.log('Before push', arr);
 
     arr.push(new Rectangle(7.5,2.5));
-    console.log(arr);
+    document.getElementById('push').appendChild(createList(arr, true, false));
 
-    document.getElementById('push').appendChild(createList(arr, true));
+    console.log('After push', arr);
+}
+
+function popSection(arr) {
+    console.log('Before pop', arr);
+
+    const popped = arr.pop();
+    document.getElementById('pop').appendChild(createList(arr, false, false));
+    const showPoppedElement = document.createElement("p");
+    showPoppedElement.textContent = `Element deleted with pop:`;
+    document.getElementById('pop').appendChild(showPoppedElement);
+    document.getElementById('pop').appendChild(createList([popped], true, false));
+
+    console.log('After pop', arr, 'item popped', popped);
+}
+
+function unshiftSection(arr) {
+    console.log('Before unshift', arr);
+
+    arr.unshift(new Rectangle(7.5,2.5));
+    document.getElementById('unshift').appendChild(createList(arr, false, true));
+
+    console.log('After unshift', arr);
+}
+
+function shiftSection(arr) {
+    console.log('Before shift', arr);
+
+    const shifted = arr.shift();
+    document.getElementById('shift').appendChild(createList(arr,false));
+    const showShiftedElement = document.createElement("p");
+    showShiftedElement.textContent = `Element deleted with shift:`;
+    document.getElementById('shift').appendChild(showShiftedElement);
+    document.getElementById('shift').appendChild(createList([shifted], false, false));
+
+    console.log('After shift', arr, 'item shifted', shifted);
+}
+
+function fifoSection(arr) {
+    pushSection(arr);
+    popSection(arr);
+}
+
+function lifoSection(arr) {
+    unshiftSection(arr);
+    shiftSection(arr);
+}
+
+
+function setup() {
+    const arr = listSection();
+    
+    fifoSection(arr);
+
+    lifoSection(arr);
 
     document.getElementById('print').innerHTML = `Rects and squares created, num of elements in array with lenght: <strong>${arr.length}</strong>`;
     
